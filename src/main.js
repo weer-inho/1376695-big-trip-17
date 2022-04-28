a7e5ecb075d81988201add32e35103bca394a861
Руслан <60976203+weer-inho@users.noreply.github.com>
2022-04-28 16:56:27 +0300

    перенес всю логику отрисовки в презентер

--- src/main.js
+++ src/main.js
@@ -5,18 +5,23 @@ import RoutePointView from './view/route-point';
 import NewFormView from './view/new-form';
 import TripEventsListView from './view/trip-events-list';
 import {render, RenderPosition} from './render';
+import BoardPresenter from './presenter/board-presenter';
 
 const body = document.querySelector('.page-body');
-const tripControls = body.querySelector('.trip-main');
-const tripControlsFilters = tripControls.querySelector('.trip-controls__filters');
-const tripEvents = body.querySelector('.trip-events');
 
-render(new InfoView(), tripControls, RenderPosition.AFTERBEGIN);
-render(new FilterView(), tripControlsFilters);
-render(new SortView(), tripEvents);
-render(new TripEventsListView(), tripEvents);
-const tripEventsList = tripEvents.querySelector('.trip-events__list');
-render(new NewFormView(), tripEventsList);
-render(new RoutePointView(), tripEventsList);
-// console.log(tripEvents);
+const boardPresenter = new BoardPresenter();
+boardPresenter.init(body);
+
+// const tripControls = body.querySelector('.trip-main');
+// const tripControlsFilters = tripControls.querySelector('.trip-controls__filters');
+// const tripEvents = body.querySelector('.trip-events');
+//
+// render(new InfoView(), tripControls, RenderPosition.AFTERBEGIN);
+// render(new FilterView(), tripControlsFilters);
+// render(new SortView(), tripEvents);
+// render(new TripEventsListView(), tripEvents);
+// const tripEventsList = tripEvents.querySelector('.trip-events__list');
+// render(new NewFormView(), tripEventsList);
+// render(new RoutePointView(), tripEventsList);
+// // console.log(tripEvents);
 
