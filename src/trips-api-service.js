0e95bea821719d4c632a45e6a970fa099d789d56
Руслан <60976203+weer-inho@users.noreply.github.com>
2022-06-07 12:40:59 +0300

    создал класс для api

---
+++ src/trips-api-service.js
@@ -0,0 +1,26 @@
+import ApiService from './framework/api-service';
+
+const Method = {
+  GET: 'GET',
+  PUT: 'PUT',
+};
+
+export default class TripsApiService extends ApiService {
+  get trips() {
+    return this._load({url: 'trips'})
+      .then(ApiService.parseResponse);
+  }
+
+  updateTrip = async (trip) => {
+    const response = await this._load({
+      url: `points/${trip.id}`,
+      method: Method.PUT,
+      body: JSON.stringify(trip),
+      headers: new Headers({'Content-type': 'application/json'}),
+    });
+
+    const parsedResponse = await ApiService.parseResponse(response);
+    return parsedResponse;
+  };
+};
+
